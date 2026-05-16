502281da0b2d4a445f0f14a2d183f2a487eb751d
smdsniffersd <164409560+smdsniffersd@users.noreply.github.com>
2026-05-16 04:31:22 +0300

    Add files via upload

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,22 +1,43 @@
-function getInfo(elem) {
+function getInfo(element) {
 
-    const value = elem.value;
-
-    fetch('/php_components/adminlogic.php', {
+    console.log('in getInfo');
+    const table = element.textContent.trim();
+    fetch('./php_components/adminlogic.php', {
         method: 'POST',
-        body: JSON.stringify({ value: value }),
+        body: JSON.stringify({ value: table }),
         headers: {
             'Content-Type': 'application/json',
             'X-Requested-With': 'XMLHttpRequest'
         }
-    }).then(responce =>{
+    }).then(responce => {
 
         return responce.json();
 
     }).then(data => {
-        if(data.success){
-            console.log('Success', data);
-        }else{
+        if (data.success) {
+            console.log(data);
+            const mainOverlay = document.getElementById('mainOverlay');
+            if (!mainOverlay) {
+                console.error('Элемент mainOverlay не найден');
+                return;
+            }
+            mainOverlay.style.display = 'block';
+            let html = '';
+            html += `<tr>`
+            for(let key in data.data[0]){
+                html += `<th>${key}</th>`;
+            }
+            html += `</tr>`;
+
+            data.data.forEach(element =>{
+                html += `<tr>`;
+                for(let key in element){
+                    html += `<td>${element[key]}</td>`;
+                }
+                html += `</tr>`;
+            });
+            mainOverlay.innerHTML = html;
+        } else {
             console.log('Error', data.message);
         }
     }).catch(error => console.error('Fetch error', error));
